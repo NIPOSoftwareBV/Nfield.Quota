3059e29a1c39e7277da9f7d78be7e0ca7281624a
bjorncoltof <bjorn.coltof@gmail.com>
2016-06-22 16:15:27 +0200

    Now?

--- prerelease.ps1
+++ prerelease.ps1
@@ -1,7 +1,7 @@
 if ($env:APPVEYOR_REPO_BRANCH -eq "master" -and (-not $env:APPVEYOR_PULL_REQUEST_NUMBER))  {
   Write-Host "Package will be published to NuGet"
-  Set-AppveyorBuildVariable -Name "assembly_informational_version" -Value "$version"
+  $env:assembly_informational_version = $env:APPVEYOR_BUILD_VERSION
 } else {
   Write-Host "Package will be published to NuGet as prerelease"
-  Set-AppveyorBuildVariable -Name "assembly_informational_version" -Value "$version-prerelease"
+  $env:assembly_informational_version =  $env:APPVEYOR_BUILD_VERSION + "-prerelease" 
 }
