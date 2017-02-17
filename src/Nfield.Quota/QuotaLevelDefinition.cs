842f16e532ab6ff82e449d987e52e198ee3dcde7
Mohamad Hafi <m.hafi@niposoftware.com>
2017-02-17 16:59:28 +0100

    Let definitions implement IEquatable

--- src/Nfield.Quota/QuotaLevelDefinition.cs
+++ src/Nfield.Quota/QuotaLevelDefinition.cs
@@ -2,9 +2,35 @@
 
 namespace Nfield.Quota
 {
-    public class QuotaLevelDefinition
+    public class QuotaLevelDefinition : IEquatable<QuotaLevelDefinition>
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+
+        public static bool operator ==(QuotaLevelDefinition existingDefinition, QuotaLevelDefinition newDefinition)
+        {
+            return existingDefinition?.Equals(newDefinition) ?? false;
+        }
+
+        public static bool operator !=(QuotaLevelDefinition existingDefinition, QuotaLevelDefinition newDefinition)
+        {
+            return !(existingDefinition == newDefinition);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as QuotaLevelDefinition;
+            return other != null && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^ Name.GetHashCode();
+        }
+
+        public bool Equals(QuotaLevelDefinition other)
+        {
+            return other != null && (Id == other.Id) && (Name == other.Name);
+        }
     }
 }
--- src/Nfield.Quota/QuotaVariableDefinition.cs
+++ src/Nfield.Quota/QuotaVariableDefinition.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Nfield.Quota
 {
-    public class QuotaVariableDefinition
+    public class QuotaVariableDefinition : IEquatable<QuotaVariableDefinition>
     {
         public QuotaVariableDefinition()
         {
@@ -21,5 +22,63 @@ namespace Nfield.Quota
         public string OdinVariableName { get; set; }
 
         public ICollection<QuotaLevelDefinition> Levels { get; }
+        
+        public static bool operator ==(QuotaVariableDefinition existingDefinition, QuotaVariableDefinition newDefinition)
+        {
+            return existingDefinition?.Equals(newDefinition) ?? false;
+        }
+
+        public static bool operator !=(QuotaVariableDefinition existingDefinition, QuotaVariableDefinition newDefinition)
+        {
+            return !(existingDefinition == newDefinition);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as QuotaVariableDefinition;
+            return other != null && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^ Name.GetHashCode() ^ OdinVariableName.GetHashCode() ^ Levels.GetHashCode();
+        }
+
+        public bool Equals(QuotaVariableDefinition other)
+        {
+            return other != null 
+                && (Id == other.Id) 
+                && (Name == other.Name) 
+                && (OdinVariableName == other.OdinVariableName) 
+                && ScrambledLevelsEquals(Levels,other.Levels);
+        }
+        private static bool ScrambledLevelsEquals<T>(IEnumerable<T> existingDefinition, IEnumerable<T> newDefinition)
+        {
+            var levels = new Dictionary<T, int>();
+            foreach (var ed in existingDefinition)
+            {
+                if (levels.ContainsKey(ed))
+                {
+                    levels[ed]++;
+                }
+                else
+                {
+                    levels.Add(ed, 1);
+                }
+            }
+            foreach (var nd in newDefinition)
+            {
+                if (levels.ContainsKey(nd))
+                {
+                    levels[nd]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return levels.Values.All(qld=> qld == 0);
+        }
+       
     }
 }
