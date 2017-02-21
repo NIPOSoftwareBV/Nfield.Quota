aef92155b5d309ccf033181eb5ac085e6bbde776
Mohamad Hafi <m.hafi@niposoftware.com>
2017-02-21 11:03:52 +0100

    make extension method internal

--- src/Nfield.Quota/Helpers/CollectionExtensions.cs
+++ src/Nfield.Quota/Helpers/CollectionExtensions.cs
@@ -29,7 +29,7 @@ namespace Nfield.Quota.Helpers
         /// <param name="left"></param>
         /// <param name="right"></param>
         /// <returns></returns>
-        public static bool ScrambledDefinitionsEquals<T>(this ICollection<T> left, ICollection<T> right)
+        internal static bool ScrambledDefinitionsEquals<T>(this ICollection<T> left, ICollection<T> right)
         {
             return left.Count == right.Count && left.All(right.Contains);
         }
