5d672cdd3db2b71001e623f59cd60cd05977e5f4
bjorncoltof <bjorn.coltof@gmail.com>
2017-04-25 15:31:14 +0200

    Sign the assembly (#32)

--- src/Nfield.Quota/Properties/AssemblyInfo.cs
+++ src/Nfield.Quota/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Nfield.Quota.Tests")]
+[assembly: InternalsVisibleTo("Nfield.Quota.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fda5c8716aad79dd1e74dfe4d6a77aa4c77b455e9b6a0d1bfab7ae703ffd1a070fbd8ad471fdd91378a8f216eb74460379d7942a7056899484d5146d6e52f93949c9020d30ed22c3cbcedbde3b7081055012c3055e5308b82fc2783babbdfa876be0d60a29c8567468295bb7e82867f61e426e238b7616b06020099160428b97")]
