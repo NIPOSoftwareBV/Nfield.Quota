70548b92b555135db1e485e53ef4fc08452f12d5
Eva del Nuevo <e.del.nuevo.munoz@nipo.com>
2018-11-12 10:15:43 +0100

    Disable only version generation

--- Nfield.Quota.Tests/Properties/AssemblyInfo.cs
+++ Nfield.Quota.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nfield.Quota.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nfield.Quota.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Nfield.Quota/Properties/AssemblyInfo.cs
+++ Nfield.Quota/Properties/AssemblyInfo.cs
@@ -5,13 +5,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nfield.Quota")]
-[assembly: AssemblyDescription("Package for quota manipulation")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NIPO Software")]
-[assembly: AssemblyProduct("Nfield")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
