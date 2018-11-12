7055c631b639b140681adbfa97efa6ec846df407
Eva del Nuevo <e.del.nuevo.munoz@nipo.com>
2018-11-12 11:56:29 +0100

    Change name

--- prerelease.ps1
+++ prerelease.ps1
@@ -1,7 +1,7 @@
 if ($env:APPVEYOR_REPO_BRANCH -eq "master" -and (-not $env:APPVEYOR_PULL_REQUEST_NUMBER))  {
   Write-Host "Package will be published to NuGet"
-  $env:csproj_informational_version = $env:APPVEYOR_BUILD_VERSION
+  $env:informational_version = $env:APPVEYOR_BUILD_VERSION
 } else {
   Write-Host "Package will be published to NuGet as prerelease"
-  $env:csproj_informational_version =  $env:APPVEYOR_BUILD_VERSION + "-prerelease" 
+  $env:informational_version =  $env:APPVEYOR_BUILD_VERSION + "-prerelease" 
 }
