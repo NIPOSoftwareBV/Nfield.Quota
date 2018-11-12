c65848c8da4bb980279d0616f08f05680e6672f4
Eva del Nuevo <e.del.nuevo.munoz@nipo.com>
2018-11-12 11:52:10 +0100

    Fix version

--- prerelease.ps1
+++ prerelease.ps1
@@ -1,7 +1,7 @@
 if ($env:APPVEYOR_REPO_BRANCH -eq "master" -and (-not $env:APPVEYOR_PULL_REQUEST_NUMBER))  {
   Write-Host "Package will be published to NuGet"
-  $env:informational_version = $env:APPVEYOR_BUILD_VERSION
+  $env:csproj_informational_version = $env:APPVEYOR_BUILD_VERSION
 } else {
   Write-Host "Package will be published to NuGet as prerelease"
-  $env:informational_version =  $env:APPVEYOR_BUILD_VERSION + "-prerelease" 
+  $env:csproj_informational_version =  $env:APPVEYOR_BUILD_VERSION + "-prerelease" 
 }
