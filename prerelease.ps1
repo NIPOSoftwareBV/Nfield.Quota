8491fab3968dcfe0439e61bbe59b67ee63f69840
Marco Bergantin <m.bergantin@nipo.com>
2019-02-05 09:51:46 +0100

    remove appveyor build file and version script (#55)

--- prerelease.ps1
+++
@@ -1,7 +0,0 @@
-if ($env:APPVEYOR_REPO_BRANCH -eq "master" -and (-not $env:APPVEYOR_PULL_REQUEST_NUMBER))  {
-  Write-Host "Package will be published to NuGet"
-  $env:package_version = $env:APPVEYOR_BUILD_VERSION
-} else {
-  Write-Host "Package will be published to NuGet as prerelease"
-  $env:package_version =  $env:APPVEYOR_BUILD_VERSION + "-prerelease" 
-}
