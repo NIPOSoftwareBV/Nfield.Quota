78a92c35810984466617290068471cf6ea455c0b
Jouke van der Maas <j.maas@niposoftware.com>
2020-01-16 15:48:59 +0100

    Add a comment explaining the interface

--- Nfield.Quota/IQuotaCell.cs
+++ Nfield.Quota/IQuotaCell.cs
@@ -2,6 +2,15 @@
 
 namespace Nfield.Quota
 {
+    //
+    // this interface is used to share logic between the quota frame (which has only a max target)
+    // and quota levels.
+    // before this interface, target-related validation logic had to be duplicated to work in
+    // both contexts.
+    // a better solution would be to have an explicit "root level" at the root of the frame, and
+    // to remove the target property from the quota frame. however, this is not a backwards compatible
+    // change, so we settle for this compromise.
+    //
     internal interface IQuotaCell
     {
         string Name { get; }
